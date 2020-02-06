b8e08fa9ceb3b9b8615ddc21db0eab0ea7891e4a
daneyul <dan.nguyen5@gmail.com>
2020-02-07 00:07:28 +0900

    pages

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import RiseRun from './sidebarSections/riserun';
+import TotalRun from './inputs/totalrun';
+import TotalRise from './inputs/totalrise';
+import IdealRun from './inputs/idealrun';
+import IdealRise from './inputs/idealrise';
 import Details from './sidebarSections/details';
-import Headroom from './sidebarSections/headroom';
 import StairAngle from './sidebarSections/stairangle';
+import Height from './inputs/height';
+import FloorOpening from './inputs/flooropening';
+import FloorThickness from './inputs/floorthickness';
 
 const Sidebar = () => {
 
 return (
 <div>
-    <RiseRun />
+    <div>
+        <div>Rise-run</div>
+        <TotalRun />
+        <TotalRise />
+        <IdealRun />
+        <IdealRise />
+    </div>
     <Details />
-    <Headroom />
+    <div>
+        <div>Headroom</div>
+        <Height />
+        <FloorOpening />
+        <FloorThickness />
+    </div>
     <StairAngle />
 </div>
   );
