fec8be5d81deacd3dce851b8a6889cedeeeb829e
Moe Amaya <moeamaya@gmail.com>
2020-03-03 14:01:35 -0700

    Change order of Webfontloader

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './Styles/App.css';
-import TotalRun from './Components/inputs/totalrun';
-import TotalRise from './Components/inputs/totalrise';
-import IdealRun from './Components/inputs/idealrun';
-import IdealRise from './Components/inputs/idealrise';
-import Stringer from './Components/inputs/stringerwidth';
-import Details from './Components/inputs/details';
-import StairAngle from './Components/inputs/stairangle';
-import Height from './Components/inputs/height';
-import FloorThickness from './Components/inputs/floorthickness';
-import Stair from './Components/stair';
-import Units from './Components/inputs/units';
-import Dimensions from './Components/inputs/dimensions';
-import DetailsToggle from './Components/inputs/detailsToggle';
-import WebFont from 'webfontloader';
-
-WebFont.load({
-	custom: {
-		families: [
-			'Söhne Mono Buch'
-			]
-		},
-		timeout: 2000
-});
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Styles/App.css";
+import TotalRun from "./Components/inputs/totalrun";
+import TotalRise from "./Components/inputs/totalrise";
+import IdealRun from "./Components/inputs/idealrun";
+import IdealRise from "./Components/inputs/idealrise";
+import Stringer from "./Components/inputs/stringerwidth";
+import Details from "./Components/inputs/details";
+import StairAngle from "./Components/inputs/stairangle";
+import Height from "./Components/inputs/height";
+import FloorThickness from "./Components/inputs/floorthickness";
+import Stair from "./Components/stair";
+import Units from "./Components/inputs/units";
+import Dimensions from "./Components/inputs/dimensions";
+import DetailsToggle from "./Components/inputs/detailsToggle";
 
 class App extends Component {
-
   state = {
     totalRunft: 12 * 12,
     totalRunin: 0,
@@ -49,7 +37,7 @@ class App extends Component {
     floorft: 0,
     floorin: 7,
     floorfr: 0,
-    
+
     headroomMax: 0,
     headroomLength: 0,
 
@@ -70,316 +58,313 @@ class App extends Component {
     dimensions: true,
     units: true,
     details: true
-
-  }
+  };
 
   //Handlers for changing total run
-  changetotalRunft = (event) => {
-      this.setState({
-        totalRunft:event.target.value,
-        runOn: true,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
-  changetotalRunin = (event) => {
-      this.setState({
-        totalRunin:event.target.value,
-        runOn: true,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
-  changetotalRunfr = (event) => {
-      this.setState({
-        totalRunfr:event.target.value,
-        runOn: true,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
+  changetotalRunft = event => {
+    this.setState({
+      totalRunft: event.target.value,
+      runOn: true,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
+  changetotalRunin = event => {
+    this.setState({
+      totalRunin: event.target.value,
+      runOn: true,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
+  changetotalRunfr = event => {
+    this.setState({
+      totalRunfr: event.target.value,
+      runOn: true,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
 
   //Handlers for changing total rise
-  changetotalRiseft = (event) => {
-      this.setState({
-        totalRiseft:event.target.value,
-        runOn: false,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
-  changetotalRisein = (event) => {
-      this.setState({
-        totalRisein:event.target.value,
-        runOn: false,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
-  changetotalRisefr = (event) => {
-      this.setState({
-        totalRisefr:event.target.value,
-        runOn: false,
-        stairToggle: false,
-        idealOn: false
-      })
-  }
+  changetotalRiseft = event => {
+    this.setState({
+      totalRiseft: event.target.value,
+      runOn: false,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
+  changetotalRisein = event => {
+    this.setState({
+      totalRisein: event.target.value,
+      runOn: false,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
+  changetotalRisefr = event => {
+    this.setState({
+      totalRisefr: event.target.value,
+      runOn: false,
+      stairToggle: false,
+      idealOn: false
+    });
+  };
 
   //Handlers for changing ideal run
-  changeidealRunin = (event) => {
+  changeidealRunin = event => {
     this.setState({
-      idealRunin:event.target.value,
+      idealRunin: event.target.value,
       stairToggle: false,
       idealOn: true
-    })
-  }
-  changeidealRunfr = (event) => {
-      this.setState({
-        idealRunfr:event.target.value,
-        stairToggle: false,
-        idealOn: true
-      })
-  }
+    });
+  };
+  changeidealRunfr = event => {
+    this.setState({
+      idealRunfr: event.target.value,
+      stairToggle: false,
+      idealOn: true
+    });
+  };
 
   //Handlers for changing ideal rise
-  changeidealRisein = (event) => {
-      this.setState({
-        idealRisein:event.target.value,
-        stairToggle: false,
-        idealOn: true
-      })
-  }
-  changeidealRisefr = (event) => {
-      this.setState({
-        idealRisefr:event.target.value,
-        stairToggle: false,
-        idealOn: true
-      })
-  }
+  changeidealRisein = event => {
+    this.setState({
+      idealRisein: event.target.value,
+      stairToggle: false,
+      idealOn: true
+    });
+  };
+  changeidealRisefr = event => {
+    this.setState({
+      idealRisefr: event.target.value,
+      stairToggle: false,
+      idealOn: true
+    });
+  };
 
   //Handlers for changing stringer
-  changestringerin = (event) => {
-    this.setState({stringerin:event.target.value})
-  }
+  changestringerin = event => {
+    this.setState({ stringerin: event.target.value });
+  };
 
-  changestringerfr = (event) => {
-    this.setState({stringerfr:event.target.value})
-  }
+  changestringerfr = event => {
+    this.setState({ stringerfr: event.target.value });
+  };
 
   //Handlers for changing floor thickness
-  changefloorft = (event) => {
-    this.setState({floorft:event.target.value})
-  }
+  changefloorft = event => {
+    this.setState({ floorft: event.target.value });
+  };
 
-  changefloorin = (event) => {
-    this.setState({floorin:event.target.value})
-  }
+  changefloorin = event => {
+    this.setState({ floorin: event.target.value });
+  };
 
-  changefloorfr = (event) => {
-    this.setState({floorfr:event.target.value})
-  }
-  
+  changefloorfr = event => {
+    this.setState({ floorfr: event.target.value });
+  };
+
   //Handlers for headroom
-  changeheadroomlength = (event) => {
+  changeheadroomlength = event => {
     this.setState({
-      headroomLength:event.target.value
-    })
-  }
+      headroomLength: event.target.value
+    });
+  };
 
   //Handlers for tread
-  changetreadin = (event) => {
+  changetreadin = event => {
     this.setState({
-      treadin:event.target.value
-    })
-  }
-  changetreadfr = (event) => {
+      treadin: event.target.value
+    });
+  };
+  changetreadfr = event => {
     this.setState({
-      treadfr:event.target.value
-    })
-  }
+      treadfr: event.target.value
+    });
+  };
 
   //Handlers for riser
-  changeriserin = (event) => {
+  changeriserin = event => {
     this.setState({
-      riserin:event.target.value
-    })
-  }
-  changeriserfr = (event) => {
+      riserin: event.target.value
+    });
+  };
+  changeriserfr = event => {
     this.setState({
-      riserfr:event.target.value
-    })
-  }
+      riserfr: event.target.value
+    });
+  };
 
-   //Handlers for nosing
-   changenosingin = (event) => {
+  //Handlers for nosing
+  changenosingin = event => {
     this.setState({
-      nosingin:event.target.value
-    })
-  }
-  changenosingfr = (event) => {
+      nosingin: event.target.value
+    });
+  };
+  changenosingfr = event => {
     this.setState({
-      nosingfr:event.target.value
-    })
-  }
+      nosingfr: event.target.value
+    });
+  };
 
   //Handlers for toggles
   changeUnitsIn = () => {
     this.setState({
       units: true
-    })
-  }
+    });
+  };
   changeUnitsMM = () => {
     this.setState({
       units: false
-    })
-  }
+    });
+  };
   changeDimensionsOn = () => {
     this.setState({
       dimensions: true
-    })
-  }
+    });
+  };
   changeDimensionsOff = () => {
     this.setState({
       dimensions: false
-    })
-  }
+    });
+  };
   changeDetailsOn = () => {
     this.setState({
       details: true
-    })
-  }
+    });
+  };
   changeDetailsOff = () => {
     this.setState({
       details: false
-    })
-  }
+    });
+  };
 
   //Handler for stair angle
-  changeStairAngle = (event) => {
+  changeStairAngle = event => {
     this.setState({
       stairToggle: true,
       stairAngle: event.target.value
-    })
-  }
+    });
+  };
 
-  render(){
+  render() {
     return (
       <div className="container-fluid">
         <div className="row">
-
           <div id="sidebar" className="col-2 ">
-              <div className="sidebar__section">
-                <Units 
-                  units = {this.state.units}
-                  changeUnitsIn = {this.changeUnitsIn}
-                  changeUnitsMM = {this.changeUnitsMM}
-                />
-                <Dimensions 
-                  dimensions = {this.state.dimensions}
-                  changeDimensionsOn = {this.changeDimensionsOn}
-                  changeDimensionsOff = {this.changeDimensionsOff}
-                />
-                <DetailsToggle 
-                  details = {this.state.details}
-                  changeDetailsOn = {this.changeDetailsOn}
-                  changeDetailsOff = {this.changeDetailsOff}
-                />
-              </div>
-
-              <div className="sidebar__section--border"></div>
-
-              <div className="sidebar__section">
-                <div className="title">Rise-run</div>
-                <TotalRun 
-                    totalRunft = {this.state.totalRunft}
-                    totalRunin = {this.state.totalRunin}
-                    totalRunfr = {this.state.totalRunfr}
-                    changetotalRunft = {this.changetotalRunft}
-                    changetotalRunin = {this.changetotalRunin}
-                    changetotalRunfr = {this.changetotalRunfr}
-                />
-                <TotalRise 
-                    totalRiseft = {this.state.totalRiseft}
-                    totalRisein = {this.state.totalRisein}
-                    totalRisefr = {this.state.totalRisefr}
-                    changetotalRiseft = {this.changetotalRiseft}
-                    changetotalRisein = {this.changetotalRisein}
-                    changetotalRisefr = {this.changetotalRisefr}
-                />
-                <IdealRun 
-                    idealRunin = {this.state.idealRunin}
-                    idealRunfr = {this.state.idealRunfr}
-                    changeidealRunin = {this.changeidealRunin}
-                    changeidealRunfr = {this.changeidealRunfr}
-                />
-                <IdealRise 
-                    idealRisein = {this.state.idealRisein}
-                    idealRisefr = {this.state.idealRisefr}
-                    changeidealRisein = {this.changeidealRisein}
-                    changeidealRisefr = {this.changeidealRisefr}
-                />
-                <Stringer 
-                  changestringerin={this.changestringerin}
-                  changestringerfr={this.changestringerfr}
-                  stringerin={this.state.stringerin}
-                  stringerfr={this.state.stringerfr}
-                />
-              </div>
-
-              <div className="sidebar__section--border"></div>
-
-              <Details 
-                treadin={this.state.treadin}
-                treadfr={this.state.treadfr}
-                riserin={this.state.riserin}
-                riserfr={this.state.riserfr}
-                nosingin={this.state.nosingin}
-                nosingfr={this.state.nosingfr}
-                changetreadin={this.changetreadin}
-                changetreadfr={this.changetreadfr}
-                changeriserin={this.changeriserin}
-                changeriserfr={this.changeriserfr}
-                changenosingin={this.changenosingin}
-                changenosingfr={this.changenosingfr}
-                details={this.state.details}
+            <div className="sidebar__section">
+              <Units
+                units={this.state.units}
+                changeUnitsIn={this.changeUnitsIn}
+                changeUnitsMM={this.changeUnitsMM}
               />
+              <Dimensions
+                dimensions={this.state.dimensions}
+                changeDimensionsOn={this.changeDimensionsOn}
+                changeDimensionsOff={this.changeDimensionsOff}
+              />
+              <DetailsToggle
+                details={this.state.details}
+                changeDetailsOn={this.changeDetailsOn}
+                changeDetailsOff={this.changeDetailsOff}
+              />
+            </div>
 
-              <div className="sidebar__section--border"></div>
+            <div className="sidebar__section--border"></div>
 
-              <div className="sidebar__section">
-                  <div className="title">Headroom</div>
-                  <Height 
-                    totalRunft={this.state.totalRunft}
-                    totalRunin={this.state.totalRunin}
-                    totalRunfr={this.state.totalRunfr}
-                    headroomLength={this.state.headroomLength}
-                    changeheadroomlength={this.changeheadroomlength}
-                  />
-                  <FloorThickness 
-                    floorft={this.state.floorft}
-                    floorin={this.state.floorin}
-                    floorfr={this.state.floorfr}
-                    changefloorft={this.changefloorft}
-                    changefloorin={this.changefloorin}
-                    changefloorfr={this.changefloorfr}
-                  />
-              </div>
-
-              <div className="sidebar__section--border"></div>
-            
-              <StairAngle
+            <div className="sidebar__section">
+              <div className="title">Rise-run</div>
+              <TotalRun
+                totalRunft={this.state.totalRunft}
+                totalRunin={this.state.totalRunin}
+                totalRunfr={this.state.totalRunfr}
+                changetotalRunft={this.changetotalRunft}
+                changetotalRunin={this.changetotalRunin}
+                changetotalRunfr={this.changetotalRunfr}
+              />
+              <TotalRise
+                totalRiseft={this.state.totalRiseft}
+                totalRisein={this.state.totalRisein}
+                totalRisefr={this.state.totalRisefr}
+                changetotalRiseft={this.changetotalRiseft}
+                changetotalRisein={this.changetotalRisein}
+                changetotalRisefr={this.changetotalRisefr}
+              />
+              <IdealRun
                 idealRunin={this.state.idealRunin}
                 idealRunfr={this.state.idealRunfr}
+                changeidealRunin={this.changeidealRunin}
+                changeidealRunfr={this.changeidealRunfr}
+              />
+              <IdealRise
                 idealRisein={this.state.idealRisein}
                 idealRisefr={this.state.idealRisefr}
-                changeStairAngle={this.changeStairAngle}
-                stairAngle={this.state.stairAngle}
+                changeidealRisein={this.changeidealRisein}
+                changeidealRisefr={this.changeidealRisefr}
               />
+              <Stringer
+                changestringerin={this.changestringerin}
+                changestringerfr={this.changestringerfr}
+                stringerin={this.state.stringerin}
+                stringerfr={this.state.stringerfr}
+              />
+            </div>
 
+            <div className="sidebar__section--border"></div>
+
+            <Details
+              treadin={this.state.treadin}
+              treadfr={this.state.treadfr}
+              riserin={this.state.riserin}
+              riserfr={this.state.riserfr}
+              nosingin={this.state.nosingin}
+              nosingfr={this.state.nosingfr}
+              changetreadin={this.changetreadin}
+              changetreadfr={this.changetreadfr}
+              changeriserin={this.changeriserin}
+              changeriserfr={this.changeriserfr}
+              changenosingin={this.changenosingin}
+              changenosingfr={this.changenosingfr}
+              details={this.state.details}
+            />
+
+            <div className="sidebar__section--border"></div>
+
+            <div className="sidebar__section">
+              <div className="title">Headroom</div>
+              <Height
+                totalRunft={this.state.totalRunft}
+                totalRunin={this.state.totalRunin}
+                totalRunfr={this.state.totalRunfr}
+                headroomLength={this.state.headroomLength}
+                changeheadroomlength={this.changeheadroomlength}
+              />
+              <FloorThickness
+                floorft={this.state.floorft}
+                floorin={this.state.floorin}
+                floorfr={this.state.floorfr}
+                changefloorft={this.changefloorft}
+                changefloorin={this.changefloorin}
+                changefloorfr={this.changefloorfr}
+              />
+            </div>
+
+            <div className="sidebar__section--border"></div>
+
+            <StairAngle
+              idealRunin={this.state.idealRunin}
+              idealRunfr={this.state.idealRunfr}
+              idealRisein={this.state.idealRisein}
+              idealRisefr={this.state.idealRisefr}
+              changeStairAngle={this.changeStairAngle}
+              stairAngle={this.state.stairAngle}
+            />
           </div>
 
           <div id="container" className="col-10">
-            <Stair 
+            <Stair
               totalRunft={this.state.totalRunft}
               totalRunin={this.state.totalRunin}
               totalRunfr={this.state.totalRunfr}
@@ -413,14 +398,10 @@ class App extends Component {
               idealBoolean={this.state.idealOn}
             />
           </div>
-        
+        </div>
       </div>
-
-    </div>
     );
   }
 }
-
-
 
 export default App;
